2c2168a650a973f0bc415d4dd2a9e38451975ff8
Vandson <vandsonsf@gmail.com>
2021-03-04 21:11:43 -0300

    estilização da homebar, acrescentado tooltip e botão settings

--- src/componets/HomeBar.tsx
+++ src/componets/HomeBar.tsx
@@ -4,16 +4,20 @@ export function HomeBar() {
   return (
     <div className={styles.container}>
       <header>
-        <div>
-          <img src="/icons/logo.svg" alt="Move.it" />
-        </div>
+        <img src="/icons/logo.svg" alt="Move.it" />
       </header>
       <main>
-        <div>
+        <div className={styles.selectedMenu}>
           <img src="/icons/homeS.svg" alt="Home" />
+          <span className={styles.tooltip}>Home</span>
         </div>
         <div>
           <img src="/icons/award.svg" alt="Leaderboard" />
+          <span className={styles.tooltip}>Leaderboard</span>
+        </div>
+        <div>
+          <img src="/icons/settings.svg" alt="Settings" />
+          <span className={styles.tooltip}>Settings</span>
         </div>
       </main>
     </div>
